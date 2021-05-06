b89707417fb86b66b456f1722a204a100068949f
Rodrigo <rodrigo.hammes7@gmail.com>
2021-05-06 18:51:01 -0300

    :art: configure ESLint and Prettier

--- src/pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-   <div>
-     <h1>Projeto UI UX Unisociesc</h1>
-   </div>
-  )
-}
---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Projeto UI UX Unisociesc</h1>
+    </div>
+  );
+}
