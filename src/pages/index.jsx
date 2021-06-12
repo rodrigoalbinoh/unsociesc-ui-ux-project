0a894846403135b92f4593c8ebf30d195a949d5b
Rodrigo <rodrigo.hammes7@gmail.com>
2021-06-11 21:16:17 -0300

    Inicia Home do site

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,137 @@
+import { Header } from '@components/Header';
+import {
+  Box,
+  Container,
+  Grid,
+  Heading,
+  LinkOverlay,
+  LinkBox,
+} from '@chakra-ui/react';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Projeto UI UX Unisociesc</h1>
-    </div>
+    <>
+      <Header />
+      <Container maxW="7xl" paddingTop="6">
+        <Box>
+          <Heading as="h2" size="lg">
+            Jogos
+          </Heading>
+          <Grid
+            gap={6}
+            marginTop={6}
+            templateColumns={['repeat(1, 1fr)', 'repeat(3, 1fr)']}
+          >
+            <LinkBox
+              as="article"
+              maxW="full"
+              background="white"
+              borderWidth="1px"
+              rounded="md"
+            >
+              <Box p="5">
+                <Heading size="md" my="2">
+                  <LinkOverlay href="/">Jogo 1</LinkOverlay>
+                </Heading>
+                <Box as="a" color="teal.400" href="/" fontWeight="bold">
+                  Leia mais
+                </Box>
+              </Box>
+            </LinkBox>
+            <LinkBox
+              as="article"
+              maxW="full"
+              background="white"
+              borderWidth="1px"
+              rounded="md"
+            >
+              <Box p="5">
+                <Heading size="md" my="2">
+                  <LinkOverlay href="/">Jogo 2</LinkOverlay>
+                </Heading>
+                <Box as="a" color="teal.400" href="/" fontWeight="bold">
+                  Leia mais
+                </Box>
+              </Box>
+            </LinkBox>
+            <LinkBox
+              as="article"
+              maxW="full"
+              background="white"
+              borderWidth="1px"
+              rounded="md"
+            >
+              <Box p="5">
+                <Heading size="md" my="2">
+                  <LinkOverlay href="/">Jogo 3</LinkOverlay>
+                </Heading>
+                <Box as="a" color="teal.400" href="/" fontWeight="bold">
+                  Leia mais
+                </Box>
+              </Box>
+            </LinkBox>
+          </Grid>
+        </Box>
+        <Box mt="9">
+          <Heading as="h2" size="lg">
+            Ranking dos Jogos
+          </Heading>
+          <Grid
+            gap={6}
+            marginTop={6}
+            templateColumns={['repeat(1, 1fr)', 'repeat(3, 1fr)']}
+          >
+            <LinkBox
+              as="article"
+              maxW="full"
+              background="white"
+              borderWidth="1px"
+              rounded="md"
+            >
+              <Box p="5">
+                <Heading size="md" my="2">
+                  <LinkOverlay href="/">Jogo 1</LinkOverlay>
+                </Heading>
+                <Box as="a" color="teal.400" href="/" fontWeight="bold">
+                  Leia mais
+                </Box>
+              </Box>
+            </LinkBox>
+            <LinkBox
+              as="article"
+              maxW="full"
+              background="white"
+              borderWidth="1px"
+              rounded="md"
+            >
+              <Box p="5">
+                <Heading size="md" my="2">
+                  <LinkOverlay href="/">Jogo 2</LinkOverlay>
+                </Heading>
+                <Box as="a" color="teal.400" href="/" fontWeight="bold">
+                  Leia mais
+                </Box>
+              </Box>
+            </LinkBox>
+            <LinkBox
+              as="article"
+              maxW="full"
+              background="white"
+              borderWidth="1px"
+              rounded="md"
+            >
+              <Box p="5">
+                <Heading size="md" my="2">
+                  <LinkOverlay href="/">Jogo 3</LinkOverlay>
+                </Heading>
+                <Box as="a" color="teal.400" href="/" fontWeight="bold">
+                  Leia mais
+                </Box>
+              </Box>
+            </LinkBox>
+          </Grid>
+        </Box>
+      </Container>
+    </>
   );
 }
