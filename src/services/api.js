78c09dd89df92a26b901d027f6196c35ee13970e
Rodrigo <rodrigo.hammes7@gmail.com>
2021-07-01 20:05:24 -0300

    remove workaround do cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL:
-    'https://thingproxy.freeboard.io/fetch/http://firebase-middleware-2.herokuapp.com',
+  baseURL: 'http://firebase-middleware-2.herokuapp.com',
 });
